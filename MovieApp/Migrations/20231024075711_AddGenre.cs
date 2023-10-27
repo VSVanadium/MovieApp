6aaa27c3078af743f586f18b64aca8a08f50ba73
vikra <vanadium.vikrant@gmail.com>
2023-10-27 09:09:57 +0200

    #102 Add in memory db context and adjust tests

---
+++ MovieApp/Migrations/20231024075711_AddGenre.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenre : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Genre",
+                table: "Movie",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Movie");
+        }
+    }
+}
--- Test/UnitTestMovies.cs
+++ Test/UnitTestMovies.cs
@@ -1,22 +1,22 @@
 using ConsoleApp.Entities;
+using NUnit.Framework;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using ConsoleApp.Services;
-
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
 
 namespace Test
 {
     [TestFixture]
     public class Tests
     {
-        private ActorDBContext _dbContext;
-        private MovieService _movieService;
-        private readonly string _connectionString = "Server=localhost\\SQLEXPRESS;Database=ActorDB;Trusted_Connection=True;TrustServerCertificate=True";
+        private ActorDBContextInMemory _dbContext;
 
         [SetUp]
         public void Setup()
         {
-            _dbContext = new ActorDBContext(_connectionString);
-            _movieService = new MovieService(_dbContext);
-
+            _dbContext = new ActorDBContextInMemory();
         }
 
         [Test]
@@ -27,7 +27,8 @@ namespace Test
             int releaseYear = 2022;
 
             // Act
-            _movieService.AddMovie(movieTitle, releaseYear);
+            _dbContext.Movies.Add(new Movie() { Id = Guid.NewGuid(), Name = movieTitle, ReleaseYear = releaseYear });
+            _dbContext.SaveChanges();
 
             // Assert
             var addedMovie = _dbContext.Movies.FirstOrDefault(m => m.Name == movieTitle && m.ReleaseYear == releaseYear);
@@ -42,7 +43,8 @@ namespace Test
             string actorName = "Michelle Yeoh";
 
             // Act
-            _movieService.AddActor(actorName);
+            _dbContext.Actors.Add(new Actor() { Name = actorName });
+            _dbContext.SaveChanges();
 
             // Assert
             var addedActor = _dbContext.Actors.FirstOrDefault(a => a.Name == actorName);
