3cfd549a1b404a3391a7e973f6b7abcf3248241f
Vikrant Saxena <104590555+VSVanadium@users.noreply.github.com>
2023-10-28 11:11:24 +0200

    Dev/101 Add seed movies (#7)
    * #103 Add seed movies

---
+++ MovieApp/Services/Seed.cs
@@ -0,0 +1,35 @@
+﻿using Bogus;
+using ConsoleApp.Data;
+using ConsoleApp.Entities;
+
+
+namespace MovieApp.Services
+{
+    public static class Seed
+    {
+        public static void SeedMovies(ActorDBContext _dbContext)
+        {
+            var fakeMovies = new Faker<Movie>()
+             .RuleFor(m => m.Id, f => f.Random.Guid())
+             .RuleFor(m => m.Name, f => f.Random.Word())
+             .RuleFor(m => m.ReleaseYear, f => f.Random.Number(1900, 2022))
+             .RuleFor(m => m.Rating, f => f.Random.Enum<Rating>());
+
+
+            var moviesToBeAdded = fakeMovies.Generate(10); // Generate 10 fake movies
+            _dbContext.Movies.AddRange(moviesToBeAdded);
+
+            _dbContext.SaveChanges();
+        }
+
+        public static void SeedActors(ActorDBContext _dbContext)
+        {
+           
+        }
+
+        public static void SeedMovieActorsMapping(ActorDBContext _dbContext)
+        {            
+
+        }
+    }
+}
