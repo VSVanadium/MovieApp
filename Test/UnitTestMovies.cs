6a1d039dd0a5b4d4b5ad130e1076b741593bac76
vikra <vanadium.vikrant@gmail.com>
2023-11-01 08:44:33 +0100

    #104 Add movie actor mapping test

--- Test/UnitTestMovies.cs
+++ Test/UnitTestMovies.cs
@@ -50,5 +50,37 @@ namespace Test
             var addedActor = _dbContext.Actors.FirstOrDefault(a => a.Name == actorName);
             Assert.NotNull(addedActor);
         }
+
+        [Test]
+        public void AddActor_ShouldAddMovieActorMappingToDatabase()
+        {
+            // Arrange
+            string movieTitle = "harry potter and the prisoner of azkaban";
+            int releaseYear = 2004;
+
+            string actor1Name = "Daniel Radcliffe";
+            string actor2Name = "Emma Watson";
+
+            var movie = new Movie() { Id = Guid.NewGuid(), Name = movieTitle, ReleaseYear = releaseYear };
+            var actor1 = new Actor() { Id = Guid.NewGuid(), Name = actor1Name, Age = 34 };
+            var actor2 = new Actor() { Id = Guid.NewGuid(), Name = actor2Name, Age = 31 };
+
+            _dbContext.Movies.Add(movie);
+            _dbContext.Actors.Add(actor1);
+            _dbContext.Actors.Add(actor2);
+
+            _dbContext.MovieActors.Add(new MovieActor(movie.Id, actor1.Id));
+            _dbContext.MovieActors.Add(new MovieActor(movie.Id, actor2.Id));
+
+            _dbContext.SaveChanges();
+
+            // Act
+            var result = _dbContext.Movies.Include(m => m.MovieActors).Where(x => x.MovieActors!.Any(x => x.Actor!.Name == actor1.Name)).FirstOrDefault();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.That(result?.Name, Is.EqualTo(movieTitle));
+
+        }
     }
 }
